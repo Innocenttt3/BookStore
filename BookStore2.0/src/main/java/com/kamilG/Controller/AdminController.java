fe1d47a6dd72aab17a02bee69786e8c6b8ab57c2
Innocenttt3 <122268653+Innocenttt3@users.noreply.github.com>
2024-07-03 12:30:45 +0200

    no css version

--- BookStore2.0/src/main/java/com/kamilG/Controller/AdminController.java
+++ BookStore2.0/src/main/java/com/kamilG/Controller/AdminController.java
@@ -1,12 +1,44 @@
 package com.kamilG.Controller;
 
+import com.kamilG.model.Order;
+import com.kamilG.service.OrderService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
-  @GetMapping("/admin/adminView")
+
+  @Autowired private OrderService orderService;
+
+  @GetMapping("/adminView")
   public String adminpanel() {
     return "adminView";
+  }
+
+  @GetMapping("/orders")
+  public String viewAllOrders(Model model) {
+    List<Order> orders = orderService.getAllOrders();
+    model.addAttribute("orders", orders);
+    return "adminPanel";
+  }
+
+  @PostMapping("/changeOrderStatus")
+  public String changeOrderStatus(
+      @RequestParam("orderId") Long orderId, @RequestParam("status") String status) {
+    orderService.updateOrderStatus(orderId, Order.OrderStatus.valueOf(status));
+    return "redirect:/admin/orders";
+  }
+
+  @PostMapping("/deleteOrder")
+  public String deleteOrder(@RequestParam("orderId") Long orderId) {
+    orderService.deleteOrderById(orderId);
+    return "redirect:/admin/orders";
   }
 }
--- BookStore2.0/src/main/java/com/kamilG/service/OrderService.java
+++ BookStore2.0/src/main/java/com/kamilG/service/OrderService.java
@@ -77,4 +77,24 @@ public class OrderService implements IOrderService {
     User user = userService.getCurrentUser();
     return orderRepository.findByUser(user);
   }
+
+  @Transactional
+  public List<Order> getAllOrders() {
+    return orderRepository.findAll();
+  }
+
+  @Transactional
+  public void updateOrderStatus(Long orderId, Order.OrderStatus status) {
+    Order order =
+        orderRepository
+            .findById(orderId)
+            .orElseThrow(() -> new RuntimeException("Order not found"));
+    order.setStatus(status);
+    orderRepository.save(order);
+  }
+
+  @Transactional
+  public void deleteOrderById(Long orderId) {
+    orderRepository.deleteById(orderId);
+  }
 }
